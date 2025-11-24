4e943ee43a50071124ca276d14559660d701e70c
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-11-24 14:27:39 +0400

    Create Data for 5 Students

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// =====================================
+// 1. Create Data for 5 Students
+// =====================================
+
+let students = [
+    { name: "Ali", age: 20, grade: 90, city: "Cairo" },
+    { name: "Sara", age: 19, grade: 82, city: "Muscat" },
+    { name: "Omar", age: 21, grade: 76, city: "Alex" },
+    { name: "Laila", age: 18, grade: 55, city: "Dubai" },
+    { name: "Huda", age: 22, grade: 40, city: "Cairo" }
+];
